302c8e543381a19d888049100c2f8d54685e28e6
Devin Austin <devin.austin@gmail.com>
2022-05-21 17:25:20 -0600

    cleanup

--- src/main/resources/static/trickle/src/App.js
+++ src/main/resources/static/trickle/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import 'video-react/dist/video-react.css';
 import React from 'react';
 import Async from "react-async";
 import { Player } from 'video-react';
@@ -18,7 +17,7 @@ const App = () => (
           return (
               <Player
                 playsInline
-                source src={`https://trickle-media.sfo3.cdn.digitaloceanspaces.com/${data.path}?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=HFR3ZTLLWMR43DB54UYV%2F20220521%2Fsfo3%2Fs3%2Faws4_request&X-Amz-Date=20220521T221447Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=8784b9b3a6260b4341b599212ab9e047c1f86ff361205d393eceacaaff17ae89`} 
+                src={`https://trickle-media.sfo3.cdn.digitaloceanspaces.com/${data.path}?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=HFR3ZTLLWMR43DB54UYV%2F20220521%2Fsfo3%2Fs3%2Faws4_request&X-Amz-Date=20220521T221447Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=8784b9b3a6260b4341b599212ab9e047c1f86ff361205d393eceacaaff17ae89`} 
               />
           );
         return null;
