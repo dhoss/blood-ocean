e07e94a594b59eb6498c96d20f4eadd62f8768ca
Devin Austin <devin.austin@gmail.com>
2022-05-21 18:31:13 -0600

    switch back to react-player

--- src/main/resources/static/trickle/src/App.js
+++ src/main/resources/static/trickle/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
 import Async from "react-async";
-import { Player } from 'video-react';
+import ReactPlayer from 'react-player'
 
 const loadVideo = () =>
   fetch("/api/videos/fixed-trimmed-end-sample.mp4")
@@ -15,10 +15,16 @@ const App = () => (
         if (error) return `Something went wrong: ${error.message}`;
         if (data)
           return (
-              <Player
-                playsInline
-                src={`https://trickle-media.sfo3.cdn.digitaloceanspaces.com/${data.path}?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=HFR3ZTLLWMR43DB54UYV%2F20220521%2Fsfo3%2Fs3%2Faws4_request&X-Amz-Date=20220521T221447Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=8784b9b3a6260b4341b599212ab9e047c1f86ff361205d393eceacaaff17ae89`} 
-              />
+            <div className='player-wrapper'>
+                <ReactPlayer 
+                    url={`https://trickle-media.sfo3.cdn.digitaloceanspaces.com/${data.path}?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=HFR3ZTLLWMR43DB54UYV%2F20220521%2Fsfo3%2Fs3%2Faws4_request&X-Amz-Date=20220521T221447Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=8784b9b3a6260b4341b599212ab9e047c1f86ff361205d393eceacaaff17ae89`} 
+                    controls={true}
+                    className="react-player"
+                    playing
+                    width="100%"
+                    height="100%"
+                />
+            </div>
           );
         return null;
       }}
