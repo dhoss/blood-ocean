0848cd7f7ba1b3f444ce28b9a06ae60afbb6fea6
Devin Austin <devin.austin@gmail.com>
2022-05-30 16:06:09 -0600

    adjust modal size

--- src/main/resources/static/bocean/src/App.js
+++ src/main/resources/static/bocean/src/App.js
@@ -3,9 +3,10 @@ import React, { useState } from 'react';
 import { useAsync } from "react-async";
 import ReactDOM from 'react-dom';
 import ReactPlayer from 'react-player'
-import 'react-responsive-modal/styles.css';
 import { Modal } from 'react-responsive-modal';
 
+import 'react-responsive-modal/styles.css';
+import './video-modal.css';
     
 const loadVideo = () =>
   fetch("/api/videos/fixed-trimmed-end-sample.mp4")
@@ -27,15 +28,21 @@ const App = () => {
     return (
       <div className='grid place-items-center h-screen'>
           <button onClick={onOpenModal}>Open modal</button>
-          <Modal open={open} onClose={onCloseModal} center>
-            <ReactPlayer 
-                url={`https://trickle-media.sfo3.cdn.digitaloceanspaces.com/${data.path}?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=HFR3ZTLLWMR43DB54UYV%2F20220525%2Fsfo3%2Fs3%2Faws4_request&X-Amz-Date=20220525T032414Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=6311917430c8786a0cd5c53a3fc6f91415fbafb5c13477062cd7737fdfc4eb33`}
-                controls={true}
-                className="react-player"
-                playing
-                width="50%"
-                height="50%"
-            />
+          <Modal 
+            open={open} 
+            onClose={onCloseModal} 
+            center
+            classNames={{
+              modal: 'video-modal',
+            }}>
+              <ReactPlayer 
+                  url={`https://trickle-media.sfo3.cdn.digitaloceanspaces.com/${data.path}?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=HFR3ZTLLWMR43DB54UYV%2F20220525%2Fsfo3%2Fs3%2Faws4_request&X-Amz-Date=20220525T032414Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=6311917430c8786a0cd5c53a3fc6f91415fbafb5c13477062cd7737fdfc4eb33`}
+                  controls={true}
+                  className="react-player"
+                  playing
+                  width="100%"
+                  height="100%"
+              />
           </Modal>
       </div>
     );
