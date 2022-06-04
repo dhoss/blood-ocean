51431ab39493bf0dcaaf3a83c027f89aca2eada3
Devin Austin <devin.austin@gmail.com>
2022-06-03 20:36:22 -0600

    fixed s3 bullshit, app starts now

---
+++ src/main/java/in/stonecolddev/bocean/configuration/S3Config.java
@@ -0,0 +1,24 @@
+package in.stonecolddev.bocean.configuration;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+    @Bean
+    public AmazonS3 s3() {
+        AWSCredentials awsCredentials =
+                new BasicAWSCredentials("accessKey", "secretKey");
+        return AmazonS3ClientBuilder
+                .standard()
+                .withRegion("ap-south-1")
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+
+    }
+}
--- src/main/java/in/stonecolddev/bocean/video/VideoService.java
+++ src/main/java/in/stonecolddev/bocean/video/VideoService.java
@@ -1,18 +1,23 @@
 package in.stonecolddev.bocean.video;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 import org.springframework.util.MimeType;
-
 import java.time.OffsetDateTime;
 import java.util.List;
 
 @Service
 public class VideoService {
 
-    @Autowired
-    private ResourceLoader resourceLoader;
+
+    private final AmazonS3 s3Client;
+
+    public VideoService(AmazonS3 s3Client) {
+        this.s3Client = s3Client;
+    }
 
     // TODO: directory hashing (https://medium.com/eonian-technologies/file-name-hashing-creating-a-hashed-directory-structure-eabb03aa4091)
     // TODO: mark and sweep resized images (https://www.educative.io/courses/a-quick-primer-on-garbage-collection-algorithms/jy6v)
